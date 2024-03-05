ec1592b079cc9d11c87423475010a19866d7e391
peppidesu <bakker.pepijn@gmail.com>
2024-03-05 14:58:18 +0100

    feat: ESLint linting workflow (#6)
    * linting config + workflow
    
    * fix: wrong lint command in workflow
    
    * synced gitignore with frontend for consistent rewriting
    
    * added prettier
    
    * rewrote with prettier
    
    * added prettier to workflow
    
    * removed outdated docker build workflow

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
-import { enhance } from '@zenstackhq/runtime';
-import { ZenStackMiddleware } from '@zenstackhq/server/express';
-import RestApiHandler from '@zenstackhq/server/api/rest';
+import express from "express";
+import { PrismaClient } from "@prisma/client";
+import { ZenStackMiddleware } from "@zenstackhq/server/express";
+import RestApiHandler from "@zenstackhq/server/api/rest";
+// import { enhance } from "@zenstackhq/runtime";
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(express.json());
 
+// --- dummy authentication
 
+// function getSessionUser(request: express.Request) {
+//   // This is a placeholder for your auth solution
+//   return {
+//     id: "",
+//   };
+// }
 
-function getSessionUser(request: express.Request) {    
-    // This is a placeholder for your auth solution
-    return {
-        id: "",
-    };
-}
+// ---
 
-const handler = RestApiHandler({ endpoint: 'http://localhost:3000/api' });
+const handler = RestApiHandler({ endpoint: "http://localhost:3000/api" });
 
-app.use('/api', ZenStackMiddleware({ 
+app.use(
+  "/api",
+  ZenStackMiddleware({
     // switch for authentication
     /// getPrisma: (request: express.Request) => enhance(prisma, { user: getSessionUser(request) }),
     getPrisma: () => prisma,
-    
-    handler: handler 
-}));
+
+    handler: handler,
+  }),
+);
 
 app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+  console.log("Server running on http://localhost:3000");
 });
