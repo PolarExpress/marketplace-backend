3ddcbbd796496373e64e7dd531b3175d8eb882e9
peppidesu <bakker.pepijn@gmail.com>
2024-03-06 18:36:33 +0100

    comment

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 
+// THIS FILE SHOULD BE REPLACED BY jest.config.json
+// jest.config.js has some weird behavior sometimes
+
 const config = {
     clearMocks: true,
     preset: "ts-jest",
