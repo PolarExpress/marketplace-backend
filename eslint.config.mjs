d003b43ee25df649179338b32b5dabcf0ca0a04c
Job Vonk <sticcpistt@icloud.com>
2024-03-08 13:00:23 +0100

    Ran prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,28 +13,40 @@ const rule = {
   meta: {
     type: "maintenance",
     docs: {
-      description: "Ensure a specific comment exists at the top of the document",
+      description:
+        "Ensure a specific comment exists at the top of the document",
       category: "Mandatory",
       recommended: true
     },
     fixable: "code"
   },
   create(context) {
-    const commentText = '\n' +
-      ' * This program has been developed by students from the bachelor\n' +
-      ' * Computer Science at Utrecht University within the Software Project course.\n' +
-      ' *\n' +
-      ' * © Copyright Utrecht University\n' +
-      ' * (Department of Information and Computing Sciences)\n' + ' ';
+    const commentText =
+      "\n" +
+      " * This program has been developed by students from the bachelor\n" +
+      " * Computer Science at Utrecht University within the Software Project course.\n" +
+      " *\n" +
+      " * © Copyright Utrecht University\n" +
+      " * (Department of Information and Computing Sciences)\n" +
+      " ";
     return {
       Program(node) {
         const comments = context.sourceCode.getCommentsBefore(node.body[0]);
-        if (!(comments.length && comments[0].range[0] === 0 && comments[0].value == commentText)) {
+        if (
+          !(
+            comments.length &&
+            comments[0].range[0] === 0 &&
+            comments[0].value == commentText
+          )
+        ) {
           context.report({
             node: comments[0] ?? node.body[0],
             message: "Copyright comment not found at the top",
             fix(fixer) {
-              return fixer.insertTextBefore(comments[0] ?? node.body[0], `/*${commentText}*/\n\n`);
+              return fixer.insertTextBefore(
+                comments[0] ?? node.body[0],
+                `/*${commentText}*/\n\n`
+              );
             }
           });
           return;
