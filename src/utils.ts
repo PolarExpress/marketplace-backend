8f9f98947a5f26671a612e28e66267e877778488
peppidesu <bakker.pepijn@gmail.com>
2024-03-08 13:45:49 +0000

    async catch util

--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,10 @@
  * (Department of Information and Computing Sciences)
  */
 
+import { NextFunction, Request, Response } from "express";
+
+type EndpointFn = (req: Request, res: Response) => Promise<void>;
+
 /**
  * `throw` wrapped in a function, so we can use it in null coalescing statements.
  *
@@ -14,3 +18,8 @@
 export const throwFn = (e: Error): never => {
   throw e;
 };
+
+export const asyncCatch =
+  (fn: EndpointFn) => (req: Request, res: Response, next: NextFunction) => {
+    fn(req, res).catch(next);
+  };
