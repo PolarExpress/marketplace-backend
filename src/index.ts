f6b3f69fb33ac40bd19d3dc98393bd5e0df7fc4e
peppidesu <bakker.pepijn@gmail.com>
2024-03-26 09:45:27 +0100

    Changed backend port to 3002

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,8 @@ import { buildApp } from "./app";
 // Create the app and start the server
 const app = buildApp(createContext());
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3002");
+const port = parseInt(process.env.MP_BACKEND_PORT ?? "3002", 10);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
