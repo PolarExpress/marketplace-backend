6a8c5ccaf6edb6fccdbc059f1061c81ad04116d6
zebby09 <z.n.h.josephusjitta@students.uu.nl>
2024-04-22 16:12:06 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -46,6 +46,7 @@ export class App {
 export function buildExpress(ctx: Context): Express {
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
   app.post("/addons/get", expressHandler(getAddonsHandler(ctx)));
   app.post("/addons/get-by-id", expressHandler(getAddonByIdHandler(ctx)));
