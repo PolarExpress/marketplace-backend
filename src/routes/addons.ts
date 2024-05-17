d9b23a0a0c69a70eaf3f1ef39bb025157ac27469
zebby09 <z.n.h.josephusjitta@students.uu.nl>
2024-05-17 11:32:57 +0200

    ehh

--- src/routes/addons.ts
+++ src/routes/addons.ts
@@ -15,7 +15,7 @@ import { Addon, AddonCategory } from "../types";
 import { throwFunction } from "../utils";
 
 // TODO: move this to a better place
-const pageSize = 2;
+const pageSize = 20;
 
 ////////////////////////////////////////////////////////////////////////////////
 
