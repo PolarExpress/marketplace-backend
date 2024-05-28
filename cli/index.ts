17ee88e1a706716852954af475790db8a1990c5f
Job Vonk <sticcpistt@icloud.com>
2024-05-28 15:35:03 +0200

    feat: Added cli as replacement for load_addons.ts (#90)
    * feat: publish cli for a single visualisation addon
    
    * Added local load and reset commands to cli
    
    * Ran prettier
    
    * fixed lockfile

---
+++ cli/index.ts
@@ -0,0 +1,39 @@
+/*
+ * This program has been developed by students from the bachelor
+ * Computer Science at Utrecht University within the Software Project course.
+ *
+ * © Copyright Utrecht University
+ * (Department of Information and Computing Sciences)
+ */
+
+import "dotenv/config";
+import yargs from "yargs";
+
+import { local } from "./local";
+import { publish } from "./publish";
+import { reset } from "./reset";
+
+(async () => {
+  await yargs
+    .scriptName("pnpm cli")
+    .command(
+      "publish <url>",
+      "Publish an addon",
+      { url: { required: true, string: true } },
+      publish
+    )
+    .command(
+      "reset",
+      "Resets the database and minio with default addons",
+      {},
+      reset
+    )
+    .command(
+      "local <path>",
+      "Publish a local addon",
+      { path: { required: true, string: true } },
+      local
+    )
+    .help()
+    .parseAsync();
+})();
---
+++ cli/local.ts
@@ -0,0 +1,110 @@
+/*
+ * This program has been developed by students from the bachelor
+ * Computer Science at Utrecht University within the Software Project course.
+ *
+ * © Copyright Utrecht University
+ * (Department of Information and Computing Sciences)
+ */
+
+import * as minio from "minio";
+import { Db, MongoClient } from "mongodb";
+import { exec } from "node:child_process";
+import { readFile, readdir } from "node:fs/promises";
+import path from "node:path";
+import { promisify } from "node:util";
+import { z } from "zod";
+
+import environment from "../src/environment";
+import { AddonCategory } from "../src/types";
+
+interface LocalArgv {
+  path: string;
+}
+
+const pexec = promisify(exec);
+
+async function createAuthor(database: Db) {
+  const authors = database.collection("authors");
+  try {
+    const author = await authors.findOne();
+    return author;
+  } catch {
+    const insertedDocument = await authors.insertOne({
+      userId: ""
+    });
+    return { _id: insertedDocument.insertedId, userId: "" };
+  }
+}
+
+export async function local(argv: LocalArgv) {
+  const manifestPath = path.join(argv.path, "manifest.json");
+  const manifestContents = await readFile(manifestPath).then(data =>
+    data.toString()
+  );
+  const unparsedManifest = JSON.parse(manifestContents) as unknown;
+  const manifest = parseManifest(unparsedManifest);
+
+  if (manifest === null) throw new Error("Invalid manifest file");
+
+  const minioClient = new minio.Client({
+    accessKey: environment.MINIO_ACCESSKEY,
+    endPoint: environment.MINIO_ENDPOINT,
+    port: environment.MINIO_PORT,
+    secretKey: environment.MINIO_SECRETKEY,
+    useSSL: false
+  });
+
+  if (!(await minioClient.bucketExists("addons")))
+    await minioClient.makeBucket("addons");
+
+  const mongo = await MongoClient.connect(environment.MONGO_URI);
+  const database = mongo.db(environment.MP_DATABASE_NAME);
+  const collection = database.collection("addons");
+
+  const author = await createAuthor(database);
+
+  const insertedDocument = await collection.insertOne({
+    authorId: author!._id,
+    category: manifest.category,
+    name: manifest.name,
+    summary: manifest.summary
+  });
+  const id = insertedDocument.insertedId;
+
+  console.log("Installing dependencies and building project");
+  await pexec(`cd ${argv.path} && pnpm i && pnpm build`);
+
+  const readmePath = path.join(argv.path, "README.md");
+  await minioClient.putObject(
+    "addons",
+    `${id}/README.md`,
+    await readFile(readmePath)
+  );
+
+  const buildPath = path.join(argv.path, "dist");
+  for (const file of await readdir(buildPath, { recursive: true })) {
+    if (/\.\w+$/.test(file)) {
+      console.log(`Uploading ${id}/${file}`);
+      const buffer = await readFile(path.join(buildPath, file));
+      await minioClient.putObject("addons", `${id}/${file}`, buffer);
+    }
+  }
+
+  await mongo.close();
+}
+
+interface Manifest {
+  category: AddonCategory;
+  name: string;
+  summary: string;
+}
+
+const categories = z.nativeEnum(AddonCategory);
+
+const manifestSchema = z.object({
+  category: categories,
+  name: z.string(),
+  summary: z.string()
+});
+
+const parseManifest: (data: unknown) => Manifest | null = manifestSchema.parse;
