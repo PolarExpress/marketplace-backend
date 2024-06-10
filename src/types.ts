fa9ff09529b07375f1d46097bf69abd7adc4ca9e
zebby09 <79280847+zebby09@users.noreply.github.com>
2024-06-10 16:22:15 +0200

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -48,8 +48,8 @@ export interface Addon {
  * Defines the sorting options that can be used to retrieve addons.
  */
 export enum SortOptions {
-  ALPHABETICAL = "alphabetical",
-  INSTALL_COUNT = "installCount",
-  NONE = "none",
-  RELEVANCE = "relevance"
+  ALPHABETICAL = "Alphabetical",
+  INSTALL_COUNT = "Install Count",
+  NONE = "None",
+  RELEVANCE = "Relevance"
 }
